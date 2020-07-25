0163fc0efd54c89e13c630ea48b123e2b70f0b61
hsiehjackson <c2hsieh@ucsd.edu>
2020-07-25 23:39:59 +0800

    7/25 commit

---
+++ public/share/footer/myFooter.js
@@ -0,0 +1,28 @@
+document.getElementById("myFooter").innerHTML = 
+  '<div class="container">'+
+    '<div class="row">'+
+      '<div class="col-lg-4 col-md-6 col-sm-6">'+
+        '<div class="single-footer-widget center">'+
+          '<h6>Copyright</h6>'+
+          '<p><a class="mymail" href="/">© Cheng-Ping Hsieh 2020</a></p>'+
+        '</div>'+
+      '</div>'+
+      '<div class="col-lg-4  col-md-6 col-sm-6">'+
+        '<div class="single-footer-widget center">'+
+          '<h6>Contact</h6>'+
+          '<p><a class="mymail" href="mailto:c2hsieh@ucsd.edu"><i class="fa fa-envelope"></i> c2hsieh@ucsd.edu</a></p>'+
+        '</div>'+
+      '</div>'+
+      '<div class="col-lg-4 col-md-6 col-sm-6 social-widget">'+
+        '<div class="single-footer-widget center">'+
+          '<h6>Social Media</h6>'+
+          '<div class="footer-social d-flex align-items-center">'+
+            '<a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/cheng-ping-hsieh/"><i class="fa fa-linkedin"></i></a>'+
+            '<a target="_blank" rel="noopener noreferrer" href="https://github.com/hsiehjackson/"><i class="fa fa-github"></i></a>'+
+            '<a target="_blank" rel="noopener noreferrer" href="https://twitter.com/ChengPingHsieh"><i class="fa fa-twitter"></i></a>'+
+            '<a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/profile.php?id=100003654741486"><i class="fa fa-facebook"></i></a>'+
+          '</div>'+
+        '</div>'+
+      '</div>'+
+    '</div>'+
+  '</div>';
---
+++ public/share/navbar/myNavbar.js
@@ -0,0 +1,16 @@
+document.getElementById("myNavbar").innerHTML = 
+  '<div class="container">'+
+    '<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">'+
+      '<span class="text-white lnr lnr-menu"></span>'+
+    '</button>'+
+    '<div class="collapse navbar-collapse justify-content-end align-items-center" id="navbarSupportedContent">'+
+      '<p style="margin:0; margin-right: auto; color:#F4F4F4;; font-weight:600; font-size: 13px; text-transform: uppercase; text-align:left;">Cheng-Ping Hsieh</p>'+
+      '<ul class="navbar-nav">'+
+        '<li><a class="mylink" href="/">Home</a></li>'+
+        '<li><a class="mylink" href="/#about">About</a></li>'+
+        '<li><a class="mylink" href="/#portfolio">Portfolio</a></li>'+
+        '<li><a class="mylink" href="/#activity">Leadership</a></li>'+
+        // '<li><a class="mylink" href="public/pages/about/pdf/ChengPing.pdf">Resume</a></li>'+
+      '</ul>'+
+    '</div>'+
+  '</div>';
